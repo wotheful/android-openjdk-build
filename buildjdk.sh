84cdd5cd119b32300a7a1b15ff2afd227e54beba
wotheful <hi114514@petalmail.com>
2025-01-26 12:30:46 +0800

    Update buildjdk.sh

--- buildjdk.sh
+++ buildjdk.sh
@@ -62,7 +62,7 @@ AUTOCONF_EXTRA_ARGS+="OBJCOPY=$OBJCOPY \
   "
 
 export CFLAGS+=" -DANDROID -pipe -integrated-as -mllvm -polly -mllvm -polly-vectorizer=stripmine -mllvm -polly-invariant-load-hoisting -mllvm -polly-run-inliner -mllvm -polly-run-dce -flto=thin -femulated-tls -fwhole-program-vtables -fdata-sections -ffunction-sections -fmerge-all-constants"
-export LDFLAGS+=" -L$PWD/dummy_libs" 
+export LDFLAGS+=" -L$PWD/dummy_libs -fuse-ld=lld" 
 
 # Create dummy libraries so we won't have to remove them in OpenJDK makefiles
 mkdir -p dummy_libs
